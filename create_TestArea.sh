eb253d789d647c23603c6a1f2b56f82f9f7063f2
Dan Guest <dguest@cern.ch>
2016-02-24 21:58:57 +0100

    Move back to using trunk for JetTagTools and BTagging

--- create_TestArea.sh
+++ create_TestArea.sh
@@ -55,10 +55,10 @@ cd $TEST_AREA_NAME
 # ________________________________________________________________________
 # checkout packages (some are commented out because we may not need them)
 
-pkgco.py BTagging-00-07-58
-pkgco.py JetTagTools-01-00-83
-# pkgco.py -A BTagging
-# pkgco.py -A JetTagTools
+# pkgco.py BTagging-00-07-58
+# pkgco.py JetTagTools-01-00-83
+pkgco.py -A BTagging
+pkgco.py -A JetTagTools
 # pkgco.py JetMomentTools-00-03-20 # Athena is JetMomentTools-00-03-11-06
 # pkgco.py PileupReweighting-00-03-06 # Athena is PileupReweighting-00-03-14
 
