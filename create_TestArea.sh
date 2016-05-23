07f963324bf27e5a10d00c255d2adbfd9b5329eb
Dan Guest <dguest@cern.ch>
2016-05-23 13:22:33 +0200

    Athena version bump

--- create_TestArea.sh
+++ create_TestArea.sh
@@ -55,8 +55,9 @@ cd $TEST_AREA_NAME
 # ________________________________________________________________________
 # checkout packages (some are commented out because we may not need them)
 
-pkgco.py -A BTagging
-pkgco.py pkgco.py JetTagTools-01-00-85
+cmt co -r MVAUtils-00-00-04 Reconstruction/MVAUtils
+pkgco.py BTagging-00-07-62
+pkgco.py -A JetTagTools
 
 # git clone git@github.com:atlas-flavor-tagging-algo/xAODAthena.git
 svn co svn+ssh://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/FlavourTagPerformanceFramework/trunk/xAODAthena
--- setup_TestArea.sh
+++ setup_TestArea.sh
@@ -46,7 +46,7 @@ else
     echo "ATLAS environment is already setup, not setting up again"
 fi
 # actually setting up Athena
-asetup 20.7.3.3,AtlasDerivation,gcc48,here,64
+asetup 20.7.5.4,AtlasDerivation,gcc49,here,64
 
 # go back to where we started
 popd
