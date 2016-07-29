03ffbc32e85dad2c4ddfa097ca5323ef61e6a7ff
Dan Guest <dguest@cern.ch>
2016-07-29 17:14:14 +0200

    remove dummy sub flag

--- grid-submit-scripts/simple-grid-sub-hbb.py
+++ grid-submit-scripts/simple-grid-sub-hbb.py
@@ -8,7 +8,7 @@ SCRIPT_DIR=$(dirname $BASH_SOURCE)
 
 OPTS=" -j jobOptions_ipnn.py"
 OPTS+=" -t holistic"
-OPTS+=" -e"
+# OPTS+=" -e"
 for DS in $(cat $SCRIPT_DIR/hbb-files.txt); do
     ${SCRIPT_DIR}/simple-grid-sub.sh $OPTS -d $DS
 done
