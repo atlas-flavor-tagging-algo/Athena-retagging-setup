73a319ef890e638e059f0d120da55714cdaec147
Dan Guest <dguest@cern.ch>
2016-08-06 20:41:19 +0200

    add check for uncommitted changes before submitting jobs

--- grid-submit-scripts/simple-grid-sub.sh
+++ grid-submit-scripts/simple-grid-sub.sh
@@ -20,6 +20,10 @@ with voms).
 Note that this should be run from the directory where your job options
 live.
 
+Requires that:
+ - You're in a git-controlled directory
+ - All changes have been committed
+
 Options:
  -h: get help
  -n <number>: n files to use (default all)
@@ -29,6 +33,7 @@ Options:
  -z <file>: create / submit a gziped tarball
  -u: upload local json / calibration files
  -e: test run, just echo command
+ -f: force submit even if uncommited changes exist
 
 EOF
 }
@@ -38,7 +43,8 @@ ECHO=""
 TAG=""
 UPLOAD_LOCAL=""
 ZIP=""
-while getopts ":hn:j:d:t:z:ue" opt $@; do
+FORCE=""
+while getopts ":hn:j:d:t:z:uef" opt $@; do
     case $opt in
         h) _help; exit 1;;
         n) OPTS+=" --nFiles ${OPTARG}";;
@@ -48,12 +54,18 @@ while getopts ":hn:j:d:t:z:ue" opt $@; do
         z) ZIP=${OPTARG};;
         u) UPLOAD_LOCAL=1;;
         e) ECHO=1;;
+        f) FORCE=1;;
         # handle errors
         \?) _usage; echo "Unknown option: -$OPTARG" >&2; exit 1;;
         :) _usage; echo "Missing argument for -$OPTARG" >&2; exit 1;;
         *) _usage; echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
     esac
 done
+
+if ! git diff-index --quiet HEAD && [[ ! $FORCE ]]; then
+    echo "ERROR: uncommitted changes in local area, please commit them" >&2
+    exit 1
+fi
 
 SCRIPT_DIR=$(dirname $BASH_SOURCE)
 OUT_OPTS=$(${SCRIPT_DIR}/output-from-input-and-voms.sh $DS $TAG)
