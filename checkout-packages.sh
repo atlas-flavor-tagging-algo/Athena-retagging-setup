26261e200c6cf70c1098fdb4f5bd57872fdb0e92
Dan Guest <dguest@cern.ch>
2016-08-16 17:06:39 +0200

    compiles with these tags

--- checkout-packages.sh
+++ checkout-packages.sh
@@ -6,8 +6,8 @@ set -eu
 # checkout packages (some are commented out because we may not need them)
 
 cmt co -r MVAUtils-00-00-04 Reconstruction/MVAUtils
-pkgco.py BTagging-00-07-62
-pkgco.py JetTagTools-01-00-91
+pkgco.py BTagging-00-07-64
+pkgco.py JetTagTools-01-00-96
 
 # git clone git@github.com:atlas-flavor-tagging-algo/xAODAthena.git
 svn co svn+ssh://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/FlavourTagPerformanceFramework/tags/FlavourTagPerformanceFramework-00-00-13/xAODAthena
