8c0fb73313036ef90f321b714c753228e6bc66ea
Dan Guest <dguest@cern.ch>
2016-08-25 18:43:28 +0200

    start tracking developments in xaod trunk

--- add-rnnip.sh
+++ add-rnnip.sh
@@ -12,11 +12,6 @@ wget http://dguest.web.cern.ch/dguest/nn-tests/ipmk.json.gz
 gunzip ipmk.json.gz
 
 JO_DIR=${TestArea}/xAODAthena/run
-PATCH_DIR=${TestArea}/../job-option-patches
-
-# patch the dl1 config
-patch -p0 < $PATCH_DIR/dl1-to-ipmp.patch
-mv jobOptions_tagdl1.py jobOptions_ipmp.py
-# and relink
-ln -sf $JO_DIR/jobOptions_tagdl1.py
+# link
+ln -sf $JO_DIR/jobOptions_ipmp.py
 
