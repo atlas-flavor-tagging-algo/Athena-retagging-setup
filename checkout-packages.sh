294b7b225cdc286ee717549967380b529448fc55
Dan Guest <dguest@cern.ch>
2016-08-30 22:49:26 +0200

    move back to the JetTagTools branch

--- checkout-packages.sh
+++ checkout-packages.sh
@@ -7,7 +7,8 @@ set -eu
 
 cmt co -r MVAUtils-00-00-04 Reconstruction/MVAUtils
 pkgco.py BTagging-00-07-64
-pkgco.py JetTagTools-01-00-96
+pkgco.py JetTagTools-01-00-96-branch
+# pkgco.py JetTagTools-01-00-96
 
 # git clone git@github.com:atlas-flavor-tagging-algo/xAODAthena.git
 svn co svn+ssh://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/FlavourTagPerformanceFramework/tags/FlavourTagPerformanceFramework-00-00-13/xAODAthena
