ad80b9c1ce75384732024d3ddb75392032573deb
Dan Guest <dguest@cern.ch>
2016-09-12 20:26:15 +0200

    more updates updates for iprnn submit script

--- grid-submit-scripts/ftag-grid-sub-rnnip.sh
+++ grid-submit-scripts/ftag-grid-sub-rnnip.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
+set -eu
 
 SCRIPT_DIR=$(dirname $BASH_SOURCE)
+JO=jobOptions_ipmp.py
+DS=mc15_13TeV:mc15_13TeV.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.merge.DAOD_FTAG2.e3698_s2608_s2183_r7725_r7676_p2625/
 
-${SCRIPT_DIR}/ftag-grid-sub.sh\
-             -j jobOptions_ipnn.py\
-             -d mc15_13TeV:mc15_13TeV.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.merge.DAOD_FTAG2.e3698_s2608_s2183_r7725_r7676_p2625/\
-             -t ipnn\
-             -e
+${SCRIPT_DIR}/ftag-grid-sub.sh -j $JO -d $DS -u -t r1
+
 
 
